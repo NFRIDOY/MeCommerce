b9dffce57bfbef4e77b0e26ab15f47160c68051d
NF RIDOY <nfridoy@gmail.com>
2024-11-02 07:05:05 +0600

    🔧 [config] redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import StoreProvider from "./StoreProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,9 +28,10 @@ export default function RootLayout({
     <html lang="en" data-theme="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <div>
+          <StoreProvider>
+            
           {children}
-          </div>
+          </StoreProvider>
       </body>
     </html>
   );
