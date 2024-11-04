b6db9ab0a34282ff7ebbce53d79aa939a48d912f
NF RIDOY <nfridoy@gmail.com>
2024-11-04 23:00:13 +0600

    ✅[Update] banner

---
+++ src/utils/exports/banners.ts
@@ -0,0 +1,7 @@
+import b90 from "../../assets/images/BannerImages/b90.jpg";
+import b91 from "../../assets/images/BannerImages/b91.jpg";
+import b92 from "../../assets/images/BannerImages/b92.jpg";
+
+const bannerList = [b90, b91, b92];
+
+export { bannerList }
