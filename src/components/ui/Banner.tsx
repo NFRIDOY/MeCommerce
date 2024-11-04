f30626b3faf538f454cbe0171008dc6847fe3938
NF RIDOY <nfridoy@gmail.com>
2024-11-04 23:12:09 +0600

    ✅[Update] banner img

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -1,9 +1,18 @@
+import { bannerList } from '@/utils/exports/banners';
+import Image from 'next/image';
 import React from 'react';
 
 const Banner = () => {
     return (
         <div>
-            
+            <Image
+            src={bannerList[1]}
+            alt='banner'
+            width={1200}
+            height={1200}
+            layout="responsive"
+            sizes="(max-width: 768px) 100vw, 50vw"
+            />
         </div>
     );
 };
--- src/utils/exports/banners.ts
+++ src/utils/exports/banners.ts
@@ -1,6 +1,6 @@
-import b90 from "../../assets/images/BannerImages/b90.jpg";
-import b91 from "../../assets/images/BannerImages/b91.jpg";
-import b92 from "../../assets/images/BannerImages/b92.jpg";
+import b90 from "@/assets/images/BannerImages/b90.jpg";
+import b91 from "@/assets/images/BannerImages/b91.jpg";
+import b92 from "@/assets/images/BannerImages/b92.jpg";
 
 const bannerList = [b90, b91, b92];
 
