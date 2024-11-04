05f7bdcf0e45f4e9f8f6751ce056667ce5313966
NF RIDOY <nfridoy@gmail.com>
2024-11-04 21:35:15 +0600

    ✅[Update] metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,35 +4,32 @@ import "./globals.css";
 import StoreProvider from "./StoreProvider";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "Me-Commerce",
-  description: "Your trusted e-Commerce website",
+    title: "MeCommerce",
+    description: "Your trusted e-Commerce website",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" data-theme="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <StoreProvider>
-            
-          {children}
-          </StoreProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" data-theme="dark">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <StoreProvider>{children}</StoreProvider>
+            </body>
+        </html>
+    );
 }
