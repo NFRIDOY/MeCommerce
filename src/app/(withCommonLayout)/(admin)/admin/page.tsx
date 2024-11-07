a4b4c7e8c02c4b52be5a5df502ddeb51bb9a5b02
NF RIDOY <nfridoy@gmail.com>
2024-11-07 17:37:22 +0600

    ✅[updated] Welcome To Admin Pannel

--- src/app/(withCommonLayout)/(admin)/admin/page.tsx
+++ src/app/(withCommonLayout)/(admin)/admin/page.tsx
@@ -1,34 +1,35 @@
+import Link from "next/link";
 import React from "react";
 // import AdminDashboard from '@/components/admin/AdminDashboard';
 
 const Admin = () => {
     return (
-        <div className="w-full">
+        <div className="w-full h-full border-2 flex flex-col justify-center">
             {/* <AdminDashboard /> */}
             <h1 className="text-center w-full text-6xl my-10">
                 Welcome To Admin Pannel
             </h1>
             <section className="text-center my-20">
                 <ul className="menu bg-base-200 lg:menu-horizontal rounded-box">
-                    <li>
-                        <a>
+                    <li className="">
+                        <Link href="/admin/category">
                             
                             Manage Category
-                        </a>
+                        </Link>
                     </li>
                     <li>
-                        <a>
+                        <Link href="/admin/products">
                             
                             Manage Products
                             
-                        </a>
+                        </Link>
                     </li>
                     <li>
-                        <a>
+                        <Link href="/admin/orders">
                             
                             Manage Orders
                             
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </section>
--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 
 const Sidebar = () => {
-    const menu = (
+    const adminMenu = (
         <>
             <li>
                 <Link href={"/admin"}>Dashboard</Link>
@@ -14,13 +14,16 @@ const Sidebar = () => {
                 <Link href={"/admin/products"}>Products</Link>
             </li>
             <li>
+                <Link href={"/admin/orders"}>Orders</Link>
+            </li>
+            <li>
                 <Link href={"/"}>Logout</Link>
             </li>
         </>
     );
     return (
         <div className="h-screen">
-            <ul className="menu bg-base-200 h-screen w-56">{menu}</ul>
+            <ul className="menu bg-base-200 h-screen w-56">{adminMenu}</ul>
         </div>
     );
 };
